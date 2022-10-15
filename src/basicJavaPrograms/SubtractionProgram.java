7ff8d181ccbbb496e09e087121b70b1b9f183103
ssjanani <ssjanani245@gmail.com>
2022-10-15 21:53:08 +0530

    Add addition program

---
+++ src/basicJavaPrograms/SubtractionProgram.java
@@ -0,0 +1,13 @@
+package basicJavaPrograms;
+
+public class SubtractionProgram {
+
+	public static void main(String[] args) {
+		int a=20;
+		int b=10;
+		int c =a-b;
+		System.out.println(c);
+
+	}
+
+}
