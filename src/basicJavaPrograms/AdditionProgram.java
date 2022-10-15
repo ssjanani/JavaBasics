ebb6cede3d99baf3b74efea7a197385838770eab
ssjanani <ssjanani245@gmail.com>
2022-10-15 21:49:35 +0530

    Adding the addition program

---
+++ src/basicJavaPrograms/AdditionProgram.java
@@ -0,0 +1,13 @@
+package basicJavaPrograms;
+
+public class AdditionProgram {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c =a+b;
+		System.out.println(c);
+
+	}
+
+}
