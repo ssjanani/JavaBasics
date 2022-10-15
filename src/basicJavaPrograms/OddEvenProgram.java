747709358ad60d48c205e178119f1d82a02e0c67
ssjanani <ssjanani245@gmail.com>
2022-10-15 21:26:21 +0530

    Commit

---
+++ src/basicJavaPrograms/OddEvenProgram.java
@@ -0,0 +1,18 @@
+package basicJavaPrograms;
+
+public class OddEvenProgram {
+
+	public static void main(String[] args) {
+		int a=10;
+		if (a%2==0)
+		{
+			System.out.println("The given number is even");
+		}
+		else
+		{
+			System.out.println("The given number is odd");
+		}
+
+	}
+
+}
