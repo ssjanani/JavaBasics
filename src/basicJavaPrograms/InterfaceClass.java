83d6c51f824e1c898c519e3176525ec7b7c2220c
ssjanani <ssjanani245@gmail.com>
2022-10-22 15:28:59 +0530

    commit to local

---
+++ src/basicJavaPrograms/InterfaceClass.java
@@ -0,0 +1,83 @@
+package basicJavaPrograms;
+
+public class InterfaceClass implements Interface1, Interface2, Interface3, Interface4{
+
+	@Override
+	public void multiplication() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int addition() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int subtraction() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int division1() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int modulus() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int add() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
+
+	
+
+	
+	
+
+	
+
+	
+
+	
+
+	
+
+	
+
+	
+
+	
+	
+	
+	
+
+	
+
+	
+
+
+	
+
+	
+
+	
+
+	
+	
+
+	
+
+	
+
+
