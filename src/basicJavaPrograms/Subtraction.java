f5133a6cef4cfafb747da55b9cef04aff75e647e
ssjanani <ssjanani245@gmail.com>
2022-10-22 16:40:24 +0530

    commit

--- src/basicJavaPrograms/Subtraction.java
+++ src/basicJavaPrograms/Subtraction.java
@@ -2,9 +2,9 @@ package basicJavaPrograms;
 
 public class Subtraction {
 	public static void main(String[] args) {
-		int a=20;
+		int n=20;
 		int b=10;
-		int c =a-b;
+		int c =n-b;
 		System.out.println(c);
 
 	}
