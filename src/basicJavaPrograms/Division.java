d846f03c2bbc0fb6776254ce2f14b83d768a10ff
ssjanani <ssjanani245@gmail.com>
2022-10-22 16:27:11 +0530

    commit

--- src/basicJavaPrograms/Division.java
+++ src/basicJavaPrograms/Division.java
@@ -3,9 +3,9 @@ package basicJavaPrograms;
 public class Division {
 
 	public static void main(String[] args) {
-		int a=20;
+		int f=20;
 		int b=20;
-		int c =a/b;
+		int c =f/b;
 		System.out.println(c);
 
 	}
