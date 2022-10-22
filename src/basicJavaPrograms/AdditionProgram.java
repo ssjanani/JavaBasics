dfd2ff53496742218c76475f7f65af3b0deae186
ssjanani <ssjanani245@gmail.com>
2022-10-22 16:52:16 +0530

    commit

--- src/basicJavaPrograms/AdditionProgram.java
+++ src/basicJavaPrograms/AdditionProgram.java
@@ -3,10 +3,10 @@ package basicJavaPrograms;
 public class AdditionProgram {
 
 	public static void main(String[] args) {
-		int m=10;
+		int bb=10;
 		int b=20;
-		int c =m+b;
-		System.out.println(m);
+		int c =bb+b;
+		System.out.println(bb);
 		System.out.println(b);
 		System.out.println(c);
 
